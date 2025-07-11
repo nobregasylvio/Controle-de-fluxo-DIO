f4a8a1faf78572d65c7edaaea70e2ee6c8a50e29
Sylvio Nobrega Soares <sylvionobregasoares@gmail.com>
2025-07-10 22:12:46 -0300

    Commit inicial

---
+++ src/dev/sylviosoares/app/exception/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package dev.sylviosoares.app.exception;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
